f5bf845676e25d072982130000e83b7c0f33ccac
reuben-nuggets <jazza.reub@gmail.com>
2022-10-02 17:46:35 +1100

    first commit

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+noun = input("Noun: ")
+celeb = input("Celberity: ")
+celeb2 = input("Celeberity: ")
+num = input("Number: ")
+exclamation = input("Exclamation: ")
+adj = input("Adjective: ")
+
+madlib = f"Cricket is like a {noun}. Did you know that {celeb} averaged {num} in test cricket. When you get someone out in cricket you should yell \
+{exclamation}. {celeb2} likes cricket becuase it is {adj}"
+
+print(madlib)
